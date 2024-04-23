1a44c364f59ab2d25ec1d9ac4233e78f0b238292
James Kaupert <jameskaupert@gmail.com>
2024-04-23 04:02:37 +0000

    start remaps

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,8 +1,1 @@
 require("jameskaupert")
-vim.opt.softtabstop=4
-vim.opt.shiftwidth=4
-vim.opt.expandtab=true
-vim.opt.smartindent=true
-vim.opt.scrolloff=8
-vim.opt.number=true
-vim.opt.relativenumber=true
---
+++ nvim/.config/nvim/lua/jameskaupert/set.lua
@@ -0,0 +1,14 @@
+vim.opt.softtabstop=4
+vim.opt.shiftwidth=4
+vim.opt.expandtab=true
+vim.opt.smartindent=true
+
+vim.opt.scrolloff=8
+
+vim.opt.number=true
+vim.opt.relativenumber=true
+
+vim.opt.signcolumn = "yes"
+
+vim.opt.colorcolumn = "80"
+
