4c249e096988dffae927b60a944616ea46d76b30
James Kaupert <jameskaupert@gmail.com>
2024-04-23 21:59:17 +0000

    more updates

--- nvim/.config/nvim/lua/jameskaupert/remap.lua
+++ nvim/.config/nvim/lua/jameskaupert/remap.lua
@@ -6,4 +6,5 @@ end)
 vim.keymap.set("i", "kj", "<Esc>")
 vim.keymap.set("v", "<leader>p", [["_dP]])
 vim.keymap.set("v", "<leader>y", [["+y]])
-
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
