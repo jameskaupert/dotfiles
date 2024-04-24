e576d949510e499c4e724d071851cb30c0369105
James Kaupert <jameskaupert@gmail.com>
2024-04-24 22:38:53 +0000

    more lsp updates

--- nvim/.config/nvim/lua/jameskaupert/lazy/lsp.lua
+++ nvim/.config/nvim/lua/jameskaupert/lazy/lsp.lua
@@ -17,5 +17,49 @@ return {
             -- to learn the available actions
             lsp_zero.default_keymaps({buffer = bufnr})
         end)
+
+        require('mason').setup({})
+        require('mason-lspconfig').setup({
+            handlers = {
+                function(server_name)
+                    require('lspconfig')[server_name].setup({})
+                end,
+                lua_ls = function()
+                  local lua_opts = lsp_zero.nvim_lua_ls()
+                  require('lspconfig').lua_ls.setup(lua_opts)
+                end,
+            }
+        })
+
+        ---
+        -- Autocompletion config
+        ---
+        local cmp = require('cmp')
+
+        local cmp_action = lsp_zero.cmp_action()
+
+
+        cmp.setup({
+          mapping = cmp.mapping.preset.insert({
+            -- `Enter` key to confirm completion
+            ['<CR>'] = cmp.mapping.confirm({select = false}),
+
+            -- Ctrl+Space to trigger completion menu
+            ['<C-Space>'] = cmp.mapping.complete(),
+
+            -- Navigate between snippet placeholder
+            ['<C-f>'] = cmp_action.luasnip_jump_forward(),
+            ['<C-b>'] = cmp_action.luasnip_jump_backward(),
+
+            -- Scroll up and down in the completion documentation
+            ['<C-u>'] = cmp.mapping.scroll_docs(-4),
+            ['<C-d>'] = cmp.mapping.scroll_docs(4),
+          }),
+          snippet = {
+            expand = function(args)
+              require('luasnip').lsp_expand(args.body)
+            end,
+          },
+        })
     end
 }
