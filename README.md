207213d8ba20b48612a3690f3b0249f85b437b5b
James Kaupert <jameskaupert@gmail.com>
2024-04-24 16:38:38 -0500

    more lsp work

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # dotfiles
+
+Work in progress...
+
+## Installation (Unix)
+1. `cd ~`
+2. Run `git clone https://github.com/jameskaupert/dotfiles.git`
+3. `cd dotfiles`
+4. Run `./install.sh`
+
+## Installation (Windows)
+1. TODO - Deal with $Home\\AppData\\Roaming situation...
--- nvim/.config/nvim/lua/jameskaupert/lazy/lsp.lua
+++ nvim/.config/nvim/lua/jameskaupert/lazy/lsp.lua
@@ -1,10 +1,21 @@
 return {
---- Uncomment the two plugins below if you want to manage the language servers from neovim
-{'williamboman/mason.nvim'},
-{'williamboman/mason-lspconfig.nvim'},
-{'VonHeikemen/lsp-zero.nvim', branch = 'v3.x'},
-{'neovim/nvim-lspconfig'},
-{'hrsh7th/cmp-nvim-lsp'},
-{'hrsh7th/nvim-cmp'},
-{'L3MON4D3/LuaSnip'}
+    "neovim/nvim-lspconfig",
+    dependencies = {
+        'williamboman/mason.nvim',
+        'williamboman/mason-lspconfig.nvim',
+        'VonHeikemen/lsp-zero.nvim', branch = 'v3.x',
+        'neovim/nvim-lspconfig',
+        'hrsh7th/cmp-nvim-lsp',
+        'hrsh7th/nvim-cmp',
+        'L3MON4D3/LuaSnip'
+    },
+    config = function()
+        local lsp_zero = require('lsp-zero')
+
+        lsp_zero.on_attach(function(client, bufnr)
+            -- see :help lsp-zero-keybindings
+            -- to learn the available actions
+            lsp_zero.default_keymaps({buffer = bufnr})
+        end)
+    end
 }
