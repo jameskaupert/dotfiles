e4300ad6d6031521344ded4362af3c49b9cef0dd
James Kaupert <jameskaupert@gmail.com>
2024-06-26 10:27:49 -0500

    nvim diagnostics window; additional go path

--- nvim/.config/nvim/lua/jameskaupert/lazy/lsp.lua
+++ nvim/.config/nvim/lua/jameskaupert/lazy/lsp.lua
@@ -16,6 +16,9 @@ return {
             -- see :help lsp-zero-keybindings
             -- to learn the available actions
             lsp_zero.default_keymaps({buffer = bufnr})
+
+            local bufopts = { noremap = true, silent = true, buffer = bufnr }
+            vim.keymap.set("n", "<space>e", vim.diagnostic.open_float, bufopts)
         end)
 
         require('mason').setup({})
--- zsh/.zshrc
+++ zsh/.zshrc
@@ -63,6 +63,7 @@ export PATH="$PATH:$HOME/.local/bin"
 export PATH="$PATH:/opt/nvim-linux64/bin"
 export PATH="$PATH:/usr/local/go/bin"
 export PATH="$PATH:/usr/local/bin/godot"
+export PATH="$PATH:$HOME/go/bin"
 
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
