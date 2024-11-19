318883b379417fed09238128dc2171a534c3c998
James Kaupert <jameskaupert@gmail.com>
2024-11-19 10:28:19 -0600

    start setting up pokemon colorscripts for dashboard

--- nvim/.config/nvim/lua/user/lazy/snacks.lua
+++ nvim/.config/nvim/lua/user/lazy/snacks.lua
@@ -4,7 +4,20 @@ return {
   lazy = false,
   opts = {
     dashboard = {
-            enabled = true,
+      enabled = true,
+      sections = {
+        { section = "header" },
+        { section = "keys", gap = 1, padding = 1 },
+        { section = "startup" },
+        {
+          section = "terminal",
+          cmd = "pokemon-colorscripts -r --no-title; sleep .1",
+          random = 10,
+          pane = 2,
+          indent = 4,
+          height = 30,
+        },
+}
     },
     statuscolumn = { enabled = true },
   },
