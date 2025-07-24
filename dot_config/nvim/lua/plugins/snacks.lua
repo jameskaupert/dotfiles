47c12879f8c24fef62f3203ad10ed33d6f470f35
James Kaupert <jameskaupert@gmail.com>
2025-07-24 21:40:49 +0000

    remove dashboard header

--- dot_config/nvim/lua/plugins/snacks.lua
+++ dot_config/nvim/lua/plugins/snacks.lua
@@ -1,6 +1,13 @@
 return {
 	"folke/snacks.nvim",
 	opts = {
+		dashboard = {
+			preset = {
+				header = {
+					enabled = false,
+				},
+			},
+		},
 		picker = {
 			ignored = true,
 			hidden = true,
